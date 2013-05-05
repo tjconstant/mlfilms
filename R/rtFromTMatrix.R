9822125f04d3f1b8c491c3ccf15660b7683a0143
Tom Constant <t.j.constant@gmail.com>
2013-05-05 15:16:58 +0100

    comment added for future reference

--- R/rtFromTMatrix.R
+++ R/rtFromTMatrix.R
@@ -6,7 +6,7 @@ rFromTMatrix<- function(M,gamma0,gamma2){
 }
 
 tFromTMatrix<- function(M,gamma0,gamma2){
-  #remember t*conj(t) does NOT in general equal T
+  #remember t*conj(t) does NOT in general equal T, and this needs fixing.
   t<-(2*gamma0)/((gamma0*M[1,1])+(gamma0*gamma2*M[1,2])+(M[2,1])+(gamma2*M[2,2]))
   return(t)
   
