5f7cabb16b94adb3b0d557d50936d3f245dedd07
Tom Constant <t.j.constant@gmail.com>
2017-03-20 15:32:08 +0000

    removal of github api calls to avoid limit

--- R/zzz.R
+++ R/zzz.R
@@ -2,20 +2,21 @@
   
   this_release<-packageVersion("mlfilms")
   
-  GET<-httr::GET
-  content<-httr::content
+  #GET<-httr::GET
+  #content<-httr::content
   
-  latest_release<-GET("https://api.github.com/repos/tjconstant/mlfilms/releases")
+  #latest_release<-GET("https://api.github.com/repos/tjconstant/mlfilms/releases")
   
-  latest_release_content<-content(latest_release)
+  #latest_release_content<-content(latest_release)
   
-  latest_release_number<-unlist(strsplit(latest_release_content[[1]]$tag_name,"-"))[1]
+  #latest_release_number<-unlist(strsplit(latest_release_content[[1]]$tag_name,"-"))[1]
   
-  if(this_release!=latest_release_number[1]){
-    packageStartupMessage(paste("WARNING: mlfilms v",this_release," is not the current stable release (mlfilms v",latest_release_number,")",sep=""))
-  }else{
-    packageStartupMessage(paste("mlfilms v",this_release," '",latest_release_content[[1]]$name,"'",sep=""))
-  }
+  #if(this_release!=latest_release_number[1]){
+    #packageStartupMessage(paste("WARNING: mlfilms v",this_release," is not the current stable release (mlfilms v",latest_release_number,")",sep=""))
+  #}else{
+  name <- "Sexy Sam"
+    packageStartupMessage(paste("mlfilms v", this_release," '",name,"'",sep=""))
+  #}
   
 }
 
