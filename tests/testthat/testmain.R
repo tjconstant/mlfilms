a5381ae9e47ea14af5fdacef29ac56db44408305
Tom Constant <t.j.constant@gmail.com>
2017-10-31 09:51:42 +0000

    add utility function tests

--- tests/testthat/testmain.R
+++ tests/testthat/testmain.R
@@ -8,6 +8,14 @@ test_that("angle scan function works", {
   expect_equal(round(R_plasmon[1,2],2), 0.92)  
   })
 
+test_that("angle scan function works for s-polarisation", {
+  
+  layers <- list(index=c(0.13+4i),thickness=c(45e-9),repetitions= 1)
+  R_plasmon <- angle_scan(incident_medium.index = 1.5+0i,exit_medium.index = 1+0i,layers = layers, polarisation = "s")
+  
+  expect_equal(round(R_plasmon[1,2],2), 0.92)  
+})
+
 test_that("wavelength scan function works", {
   
   layers <- list(index=c(0.13+4i),thickness=c(45e-9),repetitions= 1)
@@ -22,4 +30,9 @@ test_that("dispersion scan function works", {
   R_plasmon <- dispersion_scan(incident_medium.index = 1.5+0i,exit_medium.index = 1+0i,layers = layers)
   
   expect_equal(round(R_plasmon[1,3],2), 0.96)  
+})
+
+test_that("utility functions for index to epsilon work", {
+  expect_equal(round(n(18,1) + k(18,1)*1i,2), 
+               round(sqrt(epsilon.re(4.244276, 0.117806) + 1i*epsilon.im(4.244276, 0.117806)),2))
 })
