b928eee8ba757b14b9aa14622d378ea85ecdbf68
Tom Constant <t.j.constant@gmail.com>
2017-10-31 09:51:42 +0000

    add test for different polarisation

--- tests/testthat/testmain.R
+++ tests/testthat/testmain.R
@@ -8,6 +8,14 @@ test_that("angle scan function works", {
   expect_equal(round(R_plasmon[1,2],2), 0.92)  
   })
 
+test_that("angle scan function works for s-polarisation", {
+  
+  layers <- list(index=c(0.13+4i),thickness=c(45e-9),repetitions= 1)
+  R_plasmon <- angle_scan(incident_medium.index = 1.5+0i,exit_medium.index = 1+0i,layers = layers, polarisation = "s")
+  
+  expect_equal(round(R_plasmon[1,2],2), 0.92)  
+})
+
 test_that("wavelength scan function works", {
   
   layers <- list(index=c(0.13+4i),thickness=c(45e-9),repetitions= 1)
