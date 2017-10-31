413e0bce7e98313e3973f985e1f653cbb4bc76d1
Tom Constant <t.j.constant@gmail.com>
2017-10-31 10:44:31 +0000

    tests for polarisation updated

--- tests/testthat/testmain.R
+++ tests/testthat/testmain.R
@@ -24,10 +24,26 @@ test_that("wavelength scan function works", {
   expect_equal(round(R_plasmon[1,2],2), 0.96)  
 })
 
+test_that("wavelength scan function works for s polarisation", {
+  
+  layers <- list(index=c(0.13+4i),thickness=c(45e-9),repetitions= 1)
+  R_plasmon <- wavelength_scan(incident_medium.index = 1.5+0i,exit_medium.index = 1+0i,layers = layers, polarisation="s")
+  
+  expect_equal(round(R_plasmon[1,2],2), 0.96)  
+})
+
 test_that("dispersion scan function works", {
   
   layers <- list(index=c(0.13+4i),thickness=c(45e-9),repetitions= 1)
   R_plasmon <- dispersion_scan(incident_medium.index = 1.5+0i,exit_medium.index = 1+0i,layers = layers)
+  
+  expect_equal(round(R_plasmon[1,3],2), 0.96)  
+})
+
+test_that("dispersion scan function works for s polarisation", {
+  
+  layers <- list(index=c(0.13+4i),thickness=c(45e-9),repetitions= 1)
+  R_plasmon <- dispersion_scan(incident_medium.index = 1.5+0i,exit_medium.index = 1+0i,layers = layers, polarisation = "s")
   
   expect_equal(round(R_plasmon[1,3],2), 0.96)  
 })
@@ -43,4 +59,8 @@ test_that("utility functions for rindex to  n and k", {
 
 test_that("A warning appears when using pi/2 max radians", {
   expect_warning(check_for_radians(pi/2))
+})
+
+test_that("normal completion when not using radians", {
+  expect_error(check_for_radians(0), NA)
 })
